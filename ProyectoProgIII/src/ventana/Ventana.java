c33df4e9c2bbc9669ceefb5cf7b414eb69676675
Alvaro Trigueros <alvaro.trigueros@opendeusto.es>
2026-09-23 09:34:23 +0200

    Ventana creada

---
+++ ProyectoProgIII/src/ventana/Ventana.java
@@ -0,0 +1,22 @@
+package ventana;
+
+import javax.swing.JFrame;
+
+public class Ventana extends JFrame{
+
+	private static final long serialVersionUID = 1L;
+	
+	public Ventana() {
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(320, 240);
+		
+		setTitle("ProyectoIII");
+		
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
